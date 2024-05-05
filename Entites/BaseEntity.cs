a1ef1f08ffbb06b28ced4462c28d51f50507841d
Ahmad <ahmad.salempoor@gmail.com>
2024-05-05 04:44:18 -0700

    Commit Part Of Entity Layer

---
+++ Entites/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+    public abstract class BaseEntity : BaseEntity<int>
+    {
+        
+    }
+
+}
---
+++ Entites/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public class User:BaseEntity<Guid>
+    {
+        public User()
+        {
+            IsActive = true;
+        }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset LastLoginDate { get; set; }
+
+
+    }
+
+   
+}
