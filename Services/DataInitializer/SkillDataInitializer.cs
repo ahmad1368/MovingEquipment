5176068af1a5849d39ab5ee27a074972c86190b9
Ahmad <Ahmad@DESKTOP-HL3SSI2>
2024-08-04 17:13:58 -0700

    Solve Category Error

---
+++ Services/DataInitializer/SkillDataInitializer.cs
@@ -0,0 +1,41 @@
+﻿using Data.Repositories;
+using Entites;
+using System.Linq;
+
+namespace Services.DataInitializer
+{
+    public class SkillsDataInitializer : IDataInitializer
+    {
+        private readonly IRepository<Skills> repository;
+
+        public SkillsDataInitializer(IRepository<Skills> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void InitializeData()
+        {
+            if (!repository.TableNoTracking.Any(p => p.Title == "دسته بندی اولیه 1"))
+            {
+                repository.Add(new Skills
+                {
+                    Title = "دسته بندی اولیه 1"
+                });
+            }
+            if (!repository.TableNoTracking.Any(p => p.Title == "دسته بندی اولیه 2"))
+            {
+                repository.Add(new Skills
+                {
+                    Title = "دسته بندی اولیه 2"
+                });
+            }
+            if (!repository.TableNoTracking.Any(p => p.Title == "دسته بندی اولیه 3"))
+            {
+                repository.Add(new Skills
+                {
+                    Title = "دسته بندی اولیه 3"
+                });
+            }
+        }
+    }
+}
