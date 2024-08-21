18f70223b10ce09dbd7a182e8c91007d688a5c45
ahmad <ahmad@DESKTOP-4N74P7I>
2024-08-20 23:34:29 -0700

    Add SaveChange

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,39 +1,93 @@
 ﻿using Common.Utilities;
 using Entites;
+using Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    //DbContext
     {
         public ApplicationDbContext(DbContextOptions options)
-            :base(options)
+            : base(options)
         {
-                
+
         }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
-        ////    optionsBuilder.UseSqlServer("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=MovingEquipment;Data Source=.");
-        //    optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MovingEquipment;Integrated security=true");
+        //    optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MyApiDb;Integrated Security=true");
         //    base.OnConfiguring(optionsBuilder);
         //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            var entitesAssembly = typeof(IEntity).Assembly;
-            modelBuilder.RegisterAllEntites<IEntity>(entitesAssembly);
-            modelBuilder.RegisterEntityTypeConfiguration(entitesAssembly);
+
+            var entitiesAssembly = typeof(IEntity).Assembly;
+
+            modelBuilder.RegisterAllEntites<IEntity>(entitiesAssembly);
+            modelBuilder.RegisterEntityTypeConfiguration(entitiesAssembly);
             modelBuilder.AddRestrictDeleteBehaviorConvention();
             modelBuilder.AddSequentialGuidForIdConvention();
             modelBuilder.AddPluralizingTableNameConvention();
+        }
 
+        public override int SaveChanges()
+        {
+            _cleanString();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            _cleanString();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            _cleanString();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            _cleanString();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void _cleanString()
+        {
+            var changedEntities = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+            foreach (var item in changedEntities)
+            {
+                if (item.Entity == null)
+                    continue;
+
+                var properties = item.Entity.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.CanRead && p.CanWrite && p.PropertyType == typeof(string));
+
+                foreach (var property in properties)
+                {
+                    var propName = property.Name;
+                    var val = (string)property.GetValue(item.Entity, null);
+
+                    if (val.HasValue())
+                    {
+                        var newVal = val.Fa2En().FixPersianChars();
+                        if (newVal == val)
+                            continue;
+                        property.SetValue(item.Entity, newVal, null);
+                    }
+                }
+            }
         }
     }
 }
