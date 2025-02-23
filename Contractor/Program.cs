ab4d2c8f41cab296ba271b426609a8e3bdcf28f0
ahmad1368 <ahmad.salempoor@gmail.com>
2025-02-22 22:52:48 -0800

    Migration Has Error Yet

--- Contractor/Program.cs
+++ Contractor/Program.cs
@@ -1,75 +1,67 @@
-﻿
-
-using Autofac.Core;
+﻿using Autofac.Core;
 using Common.Utilities;
 using Contractor;
 using Data;
-using Microsoft.AspNetCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using System.Globalization;
 using WebFramework.Configuration;
 using WebFramework.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// تنظیم فرهنگ پیش‌فرض
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 
-//WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
-
-// Add services to the container.
+// اضافه کردن سرویس‌ها
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-//builder.Services.AddDbContext<ApplicationDbContext>(option =>
-//{
-//    option.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
-//    //option.UseSqlServer(Configuration.GetConnectionString("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=MovingEquipment;Data Source=."));
-//});
-var connectionString = builder.Configuration.GetConnectionString("SqlServer");
-
-// ثبت DbContext در DI
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString)); // برای SQL Server
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// ساخت اپلیکیشن
 var app = builder.Build();
 
-////////////////////////////////////////
+// راه‌اندازی پایگاه داده
 app.IntializeDatabase();
 
+// مدیریت خطاها
 app.UseCustomExceptionHandler();
 
-
-
+// تنظیمات HTTPS و مسیرها
 app.UseHttpsRedirection();
-
-//app.UseElmah(_siteSetting);
-
-
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-////////////////////////////////////////
 
-// Configure the HTTP request pipeline.
+// استفاده از Swagger در محیط توسعه
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+// تنظیم مسیرها
 app.MapControllers();
 
-
+// اجرای برنامه
 app.Run();
-
---
+++ Entites/Post.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public class Posts : BaseEntity<Guid>    
+    {
+        public string Topic { get; set; }
+
+        public Guid UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+        public ICollection<Posts>? Repilies { get; set; }
+        public string description { get; set; }
+    }
+}
