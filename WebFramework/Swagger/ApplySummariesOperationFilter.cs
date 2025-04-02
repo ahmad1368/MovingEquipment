1c0daa751d60a43b4a0d25def8d40f99faf51313
ahmad1368 <ahmad.salempoor@gmail.com>
2025-04-02 02:12:25 -0700

    Go Ahead One Step In Swagger, The Actions Were Listed and Authorize Form Work But When Send Request in swagger I get Error, and have to check it

--- WebFramework/Swagger/ApplySummariesOperationFilter.cs
+++ WebFramework/Swagger/ApplySummariesOperationFilter.cs
@@ -55,8 +55,8 @@ namespace WebFramework.Swagger
                 //if (!operation.Parameters[0].Description.HasValue())
                 //    operation.Parameters[0].Description = $"A unique id for the {singularizeName}";
 
-                if (!operation.Parameters[1].Description.HasValue())
-                    operation.Parameters[1].Description = $"A {singularizeName} representation";
+                if (!operation.Parameters[0].Description.HasValue())
+                    operation.Parameters[0].Description = $"A {singularizeName} representation";
             }
             else if (IsActionName("Delete", "Remove"))
             {
--- WebFramework/Swagger/SwaggerConfigurationExtensions.cs
+++ WebFramework/Swagger/SwaggerConfigurationExtensions.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using WebFramework.Configuration;
 
 namespace WebFramework.Swagger
 {
@@ -19,81 +20,127 @@ namespace WebFramework.Swagger
         {
             Assert.NotNull(services, nameof(services));
 
-            // Add services and configuration to use Swagger
             services.AddSwaggerGen(options =>
             {
-                var xmlDocPath = Path.Combine(AppContext.BaseDirectory, "Contractor.xml");
-                // Show controller XML comments like summary
+                var xmlDocPath = System.AppDomain.CurrentDomain.BaseDirectory + @"Contractor.xml";
                 options.IncludeXmlComments(xmlDocPath, true);
 
-                options.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "API V1" });
-                options.SwaggerDoc("v2", new OpenApiInfo { Version = "v2", Title = "API V2" });
 
-                #region Filters
+
+                options.DescribeAllParametersInCamelCase();
+
+                // تعریف نسخه‌های مختلف API
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "1.0" });
+                options.SwaggerDoc("v2", new OpenApiInfo { Title = "My API", Version = "2.0" });
+
+                // تنظیم فیلتر برای نمایش نسخه‌های مختلف در Swagger
+                options.OperationFilter<ApiVersioningOperationFilter>();
                 options.OperationFilter<ApplySummariesOperationFilter>();
-                #endregion
 
-                #region Add UnAuthorized to Response
-                // Add 401 response and security requirements (Lock icon) to actions that need authorization
-                options.OperationFilter<UnauthorizedResponsesOperationFilter>(true, "Bearer");
-                #endregion
+                options.OperationFilter<UnauthorizedResponsesOperationFilter>(false, "Bearer");
 
-                #region Add Jwt Authentication
-                // Add Lockout icon on top of swagger UI page to authenticate
-                //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                //{
-                //    Type = SecuritySchemeType.OAuth2,
-                //    Scheme = "Bearer",
-                //    BearerFormat = "JWT",
-                //    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                //    In = ParameterLocation.Header,
-                //});
 
                 var isDeveloapment = services.BuildServiceProvider().GetService<IHostEnvironment>().IsDevelopment();
 
                 options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
                 {
                     Type = SecuritySchemeType.OAuth2,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    In = ParameterLocation.Path,
                     Flows = new OpenApiOAuthFlows
                     {
-                                
-                       Password = new OpenApiOAuthFlow
-                       {
-                           TokenUrl = (isDeveloapment ? new Uri("https://localhost:7247/api/v1/User/Token"): new Uri("https://MyLoadWebsite/api/v1/User/Token")),
-                           Scopes = new Dictionary<string, string>
-                            {
-                                { "api.read", "Read access to protected resources" },
-                                { "api.write", "Write access to protected resources" }
-                            }
-                       }
+
+                        Password = new OpenApiOAuthFlow
+                        {
+                            TokenUrl = (isDeveloapment ? new Uri("https://localhost:7247/api/v1/User/Token") : new Uri("https://MyLoadWebsite/api/v1/User/Token")),
+                            Scopes = new Dictionary<string, string>
+                                {
+                                    { "api.read", "Read access to protected resources" },
+                                    { "api.write", "Write access to protected resources" }
+                                }
+                        }
                     }
                 });
-
-                #endregion
-
-                
-
-
-                #region Versioning
-                // Remove version parameter from all Operations
-                options.OperationFilter<RemoveVersionParameters>();
-
-                // Set version "api/v{version}/[controller]" from current swagger doc version
-                options.DocumentFilter<SetVersionInPaths>();
-
-                // Separate and categorize end-points by doc version
-                options.DocInclusionPredicate((docName, apiDesc) =>
-                {
-                    if (!apiDesc.TryGetMethodInfo(out MethodInfo methodInfo)) return false;
-
-                    var versions = methodInfo.DeclaringType
-                        .GetCustomAttributes<ApiVersionAttribute>(true)
-                        .SelectMany(attr => attr.Versions);
-
-                    return versions.Any(v => $"v{v.ToString()}" == docName);
-                });
-                #endregion
             });
+
+            //// Add services and configuration to use Swagger
+            //services.AddSwaggerGen(options =>
+            //{
+            //    var xmlDocPath = Path.Combine(AppContext.BaseDirectory, "Contractor.xml");
+            //    // Show controller XML comments like summary
+            //    options.IncludeXmlComments(xmlDocPath, true);
+
+            //    options.SwaggerDoc("v1", new OpenApiInfo { Version = "1.0", Title = "API V1" });
+            //    options.SwaggerDoc("v2", new OpenApiInfo { Version = "2.0", Title = "API V2" });
+
+            //    #region Filters
+            //    options.OperationFilter<ApplySummariesOperationFilter>();
+            //    #endregion
+
+            //    #region Add UnAuthorized to Response
+            //    // Add 401 response and security requirements (Lock icon) to actions that need authorization
+            //    options.OperationFilter<UnauthorizedResponsesOperationFilter>(true, "Bearer");
+            //    #endregion
+
+            //    #region Add Jwt Authentication
+            //    // Add Lockout icon on top of swagger UI page to authenticate
+            //    //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    //{
+            //    //    Type = SecuritySchemeType.OAuth2,
+            //    //    Scheme = "Bearer",
+            //    //    BearerFormat = "JWT",
+            //    //    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+            //    //    In = ParameterLocation.Header,
+            //    //});
+
+            //    var isDeveloapment = services.BuildServiceProvider().GetService<IHostEnvironment>().IsDevelopment();
+
+            //    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            //    {
+            //        Type = SecuritySchemeType.OAuth2,
+            //        Flows = new OpenApiOAuthFlows
+            //        {
+
+            //           Password = new OpenApiOAuthFlow
+            //           {
+            //               TokenUrl = (isDeveloapment ? new Uri("https://localhost:7247/api/v1/User/Token"): new Uri("https://MyLoadWebsite/api/v1/User/Token")),
+            //               Scopes = new Dictionary<string, string>
+            //                {
+            //                    { "api.read", "Read access to protected resources" },
+            //                    { "api.write", "Write access to protected resources" }
+            //                }
+            //           }
+            //        }
+            //    });
+
+            //    #endregion
+
+
+
+
+            //    #region Versioning
+            //    // Remove version parameter from all Operations
+            //    options.OperationFilter<RemoveVersionParameters>();
+
+            //    // Set version "api/v{version}/[controller]" from current swagger doc version
+            //    options.DocumentFilter<SetVersionInPaths>();
+
+            //    // Separate and categorize end-points by doc version
+            //    options.DocInclusionPredicate((docName, apiDesc) =>
+            //    {
+            //        if (!apiDesc.TryGetMethodInfo(out MethodInfo methodInfo)) return false;
+
+            //        var versions = methodInfo.DeclaringType
+            //            .GetCustomAttributes<ApiVersionAttribute>(true)
+            //            .SelectMany(attr => attr.Versions);
+
+            //        return versions.Any(v => $"v{v.ToString()}" == docName);
+            //    });
+            //    #endregion
+            //});
+
         }
 
         public static void UseSwaggerAndUI(this IApplicationBuilder app)
